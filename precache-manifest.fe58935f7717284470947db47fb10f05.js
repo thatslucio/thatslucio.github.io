6b028e5e3adaf529d341d2fc01a1e6017c959473
thatslucio <lucio.cocorullo001@studenti.uniparthenope.it>
2020-11-12 19:32:50 +0100

    Updates

---
+++ precache-manifest.fe58935f7717284470947db47fb10f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5702629420ba951b7d0006d8eb3c914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54d31c917f2779453cbc",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36dc4c21ac5707915aff",
+    "url": "/static/js/2.1288cb2e.chunk.js"
+  },
+  {
+    "revision": "54d31c917f2779453cbc",
+    "url": "/static/js/main.492e3c97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a396670f9169263baeb071880a740f3",
+    "url": "/static/media/profile.8a396670.jpg"
+  }
+]);
