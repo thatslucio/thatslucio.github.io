50b65783a159812df008ce90cacb0a22ca830b23
thatslucio <lucio.cocorullo001@studenti.uniparthenope.it>
2020-11-12 21:16:05 +0100

    edited personal informations

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a5c4c8966e1fdbb75690f07aafc1a9b.js"
+  "/precache-manifest.ef66846593e6c828fdfc995d570ccca3.js"
 );
 
 self.addEventListener('message', (event) => {
